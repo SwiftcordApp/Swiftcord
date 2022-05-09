5ab9448b76577fa3a8e87fae219b8b90bda06aeb
vinkwok <cryptoalgro@gmail.com>
2022-05-09 16:29:15 +0800

    Put back top padding in List, causes UI bugs but is the best solution for now

--- Swiftcord/Views/Server/ChannelList.swift
+++ Swiftcord/Views/Server/ChannelList.swift
@@ -47,6 +47,7 @@ struct ChannelList: View {
                     .listRowInsets(.init(top: 1, leading: 16, bottom: 1, trailing: 16)) }
             }
         }
+        .padding(.top)
         .padding(.horizontal, -14) // Horrible hack to work around List's clipping to bounds
         .listStyle(.sidebar)
         .frame(minWidth: 240, maxHeight: .infinity)
