91e87df1bf55be46030eff8973e78f221e41595e
vinkwok <cryptoalgro@gmail.com>
2022-05-14 21:33:28 +0800

    Fix bug while handing reconnect op that caused permernant disconnection

--- Swiftcord/DiscordAPI/Gateway/RobustWebSocket.swift
+++ Swiftcord/DiscordAPI/Gateway/RobustWebSocket.swift
@@ -239,7 +239,7 @@ class RobustWebSocket: NSObject, ObservableObject {
             onEvent.notify(event: (type, decoded.d))
         case .reconnect:
             log.warning("Gateway-requested reconnect: disconnecting and reconnecting immediately")
-            close(code: .goingAway)
+            forceClose()
         }
     }
     
