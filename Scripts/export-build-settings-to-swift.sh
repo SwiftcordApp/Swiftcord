75563b1f5eb9872b3e4c7a73896fc6ffa10ff324
Tony Arnold <tony@thecocoabots.com>
2022-06-03 20:40:21 +1000

    Rework how the build settings are exported

--- Scripts/export-build-settings-to-swift.sh
+++ Scripts/export-build-settings-to-swift.sh
@@ -6,10 +6,12 @@ set -o nounset
 export PATH=${PATH}:/usr/local/bin
 export LC_ALL="en_US.UTF-8"
 
-BUILD_SETTINGS_OUTPUT_PATH="${BUILD_SETTINGS_OUTPUT_PATH?error: Build settings output path is not available via the 'BUILD_SETTINGS_OUTPUT_PATH' environment variable}"
+BUILD_SETTINGS_SWIFT_SOURCE_PATH="${BUILD_SETTINGS_SWIFT_SOURCE_PATH?error: Build settings Swift output path is not available via the 'BUILD_SETTINGS_SWIFT_SOURCE_PATH' environment variable}"
+
+echo "Writing build settings to ${BUILD_SETTINGS_SWIFT_SOURCE_PATH}"
 
 # Write the file contents out - if you want to export more build settings, this is the place to do it.
-cat >"${BUILD_SETTINGS_OUTPUT_PATH}" <<EOS
+cat >"${BUILD_SETTINGS_SWIFT_SOURCE_PATH}" <<EOS
 //  #### YOUR CHANGES WILL BE OVERWRITTEN! ####
 //
 //  Please do not make changes to this file - it is updated during and ignored by source control as a result of the build process.
