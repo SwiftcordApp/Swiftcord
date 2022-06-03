2c101cda2211bd87ca58ea5fc2c133b4a9771677
Tony Arnold <tony@thecocoabots.com>
2022-06-03 12:09:34 +1000

    Use the provided Calendar method to determine if two dates are on the same day

--- Swiftcord/Utils/Extensions/Date+.swift
+++ Swiftcord/Utils/Extensions/Date+.swift
@@ -9,7 +9,6 @@ import Foundation
 
 extension Date {
 	func isSameDay(as date: Date) -> Bool {
-		let diff = Calendar.current.dateComponents([.day], from: self, to: date)
-		return diff.day == 0
+		return Calendar.current.isDate(self, inSameDayAs: date)
 	}
 }
