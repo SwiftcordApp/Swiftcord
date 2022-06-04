cb385d3490ab52904a8d5b74a882a2bef15c80b6
vinkwok <cryptoalgro@gmail.com>
2022-06-04 11:41:13 +0800

    Improved debug settings

---
+++ Swiftcord/Views/Settings/Misc/DebugSettingsView.swift
@@ -0,0 +1,59 @@
+//
+//  DebugSettingsVieq.swift
+//  Swiftcord
+//
+//  Created by Vincent Kwok on 4/6/22.
+//
+
+import SwiftUI
+import DiscordKit
+
+struct DebugTableItem: Identifiable {
+	let id = UUID()
+	let item: String
+	let val: String
+}
+
+struct DebugSettingsView: View {
+	@EnvironmentObject var gateway: DiscordGateway
+	@EnvironmentObject var state: UIState
+
+    var body: some View {
+		let debugValues = [
+			/*DebugTableItem(item: "Gateway connected", val: gateway.isConnected.toString()),
+			DebugTableItem(item: "Gateway reconnecting", val: gateway.isReconnecting.toString()),
+			DebugTableItem(item: "Gateway cannot resume", val: gateway.doNotResume.toString()),
+			DebugTableItem(item: "Gateway sequence #", val: String(gateway.seq ?? 0)),
+			// DebugTableItem(item: "Gateway viability", val: gateway.viability.toString()),
+			DebugTableItem(item: "Gateway connection #", val: String(gateway.connTimes)),
+			DebugTableItem(item: "Gateway session ID", val: gateway.sessionID ?? "nil"),
+			DebugTableItem(item: "Gateway missed heartbeat ACKs", val: String(gateway.missedACK)),*/
+
+			DebugTableItem(item: "Gateway session established", val: gateway.connected.toString()),
+			DebugTableItem(item: "Network reachable", val: gateway.reachable.toString()),
+			DebugTableItem(item: "Loading stage", val: String(describing: state.loadingState)),
+			DebugTableItem(item: "Base URL", val: GatewayConfig.default.baseURL),
+			DebugTableItem(item: "REST API base URL", val: GatewayConfig.default.restBase),
+			DebugTableItem(item: "Gateway URL", val: GatewayConfig.default.gateway)
+		]
+
+		VStack(spacing: 0) {
+			Table(debugValues) {
+				TableColumn("Item", value: \.item)
+				TableColumn("Value", value: \.val)
+			}
+			Divider()
+			ScrollView {
+				VStack(alignment: .leading, spacing: 16) {
+					Text("Actions").font(.largeTitle)
+					Button("Crash Swiftcord") {
+						let funny: Int? = nil
+						print(funny!)
+					}
+				}
+				.padding()
+				.frame(maxWidth: .infinity, alignment: .leading)
+			}
+		}
+    }
+}
--- Swiftcord/Views/Settings/Misc/MiscSettingsView.swift
+++ Swiftcord/Views/Settings/Misc/MiscSettingsView.swift
@@ -6,38 +6,11 @@
 //
 
 import SwiftUI
-import DiscordKit
-
-struct DebugTableItem: Identifiable {
-    let id = UUID()
-    let item: String
-    let val: String
-}
 
 struct MiscSettingsView: View {
-    @EnvironmentObject var gateway: DiscordGateway
-    @EnvironmentObject var state: UIState
 	@State private var selectedLink: SidebarLink? = .changelog
 
     var body: some View {
-        let debugValues = [
-            /*DebugTableItem(item: "Gateway connected", val: gateway.isConnected.toString()),
-            DebugTableItem(item: "Gateway reconnecting", val: gateway.isReconnecting.toString()),
-            DebugTableItem(item: "Gateway cannot resume", val: gateway.doNotResume.toString()),
-            DebugTableItem(item: "Gateway sequence #", val: String(gateway.seq ?? 0)),
-            // DebugTableItem(item: "Gateway viability", val: gateway.viability.toString()),
-            DebugTableItem(item: "Gateway connection #", val: String(gateway.connTimes)),
-            DebugTableItem(item: "Gateway session ID", val: gateway.sessionID ?? "nil"),
-            DebugTableItem(item: "Gateway missed heartbeat ACKs", val: String(gateway.missedACK)),*/
-
-            DebugTableItem(item: "Gateway session established", val: gateway.connected.toString()),
-            DebugTableItem(item: "Network reachable", val: gateway.reachable.toString()),
-            DebugTableItem(item: "Loading stage", val: String(describing: state.loadingState)),
-            DebugTableItem(item: "Base URL", val: GatewayConfig.default.baseURL),
-            DebugTableItem(item: "REST API base URL", val: GatewayConfig.default.restBase),
-            DebugTableItem(item: "Gateway URL", val: GatewayConfig.default.gateway)
-        ]
-
         NavigationView {
 			List {
 				NavigationLink("Change Log", tag: SidebarLink.changelog, selection: $selectedLink) {
@@ -53,14 +26,7 @@ struct MiscSettingsView: View {
                 }
 
 				NavigationLink("Debug", tag: SidebarLink.debug, selection: $selectedLink) {
-                    Table(debugValues) {
-                        TableColumn("Item", value: \.item)
-                        TableColumn("Value", value: \.val)
-                    }
-					Button("Crash me (Not a joke, this will crash Swiftcord!)") {
-						let one: Int? = nil
-						print(one!)
-					}
+                    DebugSettingsView()
                 }
             }.listStyle(SidebarListStyle())
         }
