b5e4fb0f877a8acc03aa4253fded0b23f6e7f26a
Andrew Glaze <andrew.glaze76@gmail.com>
2022-06-27 14:38:39 +0000

    more CI fixes

--- ci_scripts/ci_post_xcodebuild.sh
+++ ci_scripts/ci_post_xcodebuild.sh
@@ -4,10 +4,12 @@
 mkdir -p /Users/local/Homebrew/Cellar/node-16/16/
 curl https://nodejs.org/download/release/latest-gallium/node-v16.15.1-darwin-x64.tar.xz --output node-16.tar.gz
 tar -xf node-16.tar.gz -C /Users/local/Homebrew/Cellar/node-16/16/ --strip-components=1
-/Users/local/Homebrew/Cellar/node-16/16/bin/npm install appcenter-cli
+
+export PATH="/Users/local/Homebrew/Cellar/node-16/16/bin/:$PATH"
+npm install appcenter-cli
 
 # Upload symbols to App Center
-/Users/local/Homebrew/Cellar/node-16/16/bin/npm exec appcenter crashes upload-symbols \
+npm exec appcenter crashes upload-symbols \
     --token "$APPCENTER_API_TOKEN" \
     --app "Swiftcord/Swiftcord" \
     --xcarchive "$CI_ARCHIVE_PATH"
