0ee1d4798293b1606c5dcf094442365c7ab80d37
Andrew Glaze <andrew.glaze76@gmail.com>
2022-06-27 14:13:16 +0000

    Install AppCenter CLI

--- ci_scripts/ci_post_xcodebuild.sh
+++ ci_scripts/ci_post_xcodebuild.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-# Upload symbols to App Center
+# Install AppCenter-CLI
+mkdir -p /Users/local/Homebrew/Cellar/node-16/16/
+curl https://nodejs.org/download/release/latest-gallium/node-v16.15.1-darwin-x64.tar.xz --output node-16.tar.gz
+tar -xf node-16.tar.gz -C /Users/local/Homebrew/Cellar/node-16/16/ --strip-components=1
+brew link node-16
+npm install --location=global appcenter-cli
 
+# Upload symbols to App Center
 appcenter crashes upload-symbols \
     --token $APPCENTER_API_TOKEN \
     --app "Swiftcord/Swiftcord" \
