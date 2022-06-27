9121c1b3501bc5913db37ccd5b8e5797a2b29791
Andrew Glaze <andrew.glaze76@gmail.com>
2022-06-27 14:25:53 +0000

    deal with npm weirdness

--- ci_scripts/ci_post_xcodebuild.sh
+++ ci_scripts/ci_post_xcodebuild.sh
@@ -4,11 +4,10 @@
 mkdir -p /Users/local/Homebrew/Cellar/node-16/16/
 curl https://nodejs.org/download/release/latest-gallium/node-v16.15.1-darwin-x64.tar.xz --output node-16.tar.gz
 tar -xf node-16.tar.gz -C /Users/local/Homebrew/Cellar/node-16/16/ --strip-components=1
-brew link node-16
-npm install --location=global appcenter-cli
+/Users/local/Homebrew/Cellar/node-16/16/bin/npm install appcenter-cli
 
 # Upload symbols to App Center
-appcenter crashes upload-symbols \
-    --token $APPCENTER_API_TOKEN \
+npm exec appcenter crashes upload-symbols \
+    --token "$APPCENTER_API_TOKEN" \
     --app "Swiftcord/Swiftcord" \
-    --xcarchive "Swiftcord.xcarchive"
+    --xcarchive "$CI_ARCHIVE_PATH"
