a447f96768ca0f0e35719243dea45f9ef26f90cc
Andrew Glaze <andrew.glaze76@gmail.com>
2022-06-29 13:51:10 +0000

    make the ci scripts executable

---
+++ ci_scripts/ci_post_clone.sh
@@ -0,0 +1,10 @@
+!#/bin/bash
+
+# Install AppCenter-CLI
+mkdir -p /Users/local/Homebrew/Cellar/node-16/16/
+curl https://nodejs.org/download/release/latest-gallium/node-v16.15.1-darwin-x64.tar.xz --output node-16.tar.gz
+tar -xf node-16.tar.gz -C /Users/local/Homebrew/Cellar/node-16/16/ --strip-components=1
+
+brew link node-16
+
+brew install swiftlint
