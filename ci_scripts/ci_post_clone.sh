636cc33f7e7f7ecc6d204f904687c1fa049ea098
Andrew Glaze <andrew.glaze76@gmail.com>
2022-07-08 14:18:04 +0000

    Skip installing swiftlint

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -2,9 +2,9 @@
 
 # Install AppCenter-CLI
 mkdir -p /Users/local/Homebrew/Cellar/node-16/16/
-curl https://nodejs.org/download/release/latest-gallium/node-v16.15.1-darwin-x64.tar.xz --output node-16.tar.gz
+curl https://nodejs.org/dist/v16.16.0/node-v16.16.0-darwin-x64.tar.gz --output node-16.tar.gz
 tar -xf node-16.tar.gz -C /Users/local/Homebrew/Cellar/node-16/16/ --strip-components=1
 
 brew link node-16
 
-brew install swiftlint
+# brew install swiftlint
