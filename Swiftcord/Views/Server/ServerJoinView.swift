120dea048d00de76d42088a01271f7342a4178c4
vinkwok <cryptoalgro@gmail.com>
2022-07-09 22:46:05 +0800

    Finished add server skeleton UI

--- Swiftcord/Views/Server/ServerJoinView.swift
+++ Swiftcord/Views/Server/ServerJoinView.swift
@@ -10,12 +10,31 @@ import SwiftUI
 struct ServerJoinView: View {
 	@Binding var presented: Bool
 
+	@State private var invite = ""
+
     var body: some View {
-		VStack(spacing: 32) {
+		VStack(spacing: 16) {
 			VStack(spacing: 4) {
 				Text("server.join.title").font(.title)
 				Text("server.join.caption").frame(maxWidth: .infinity, alignment: .center)
 			}
+
+			VStack(alignment: .leading, spacing: 8) {
+				Text("server.join.fieldHeader").textCase(.uppercase).font(.headline).opacity(0.75)
+				TextField("https://discord.gg/hTKzmak", text: $invite)
+					.textFieldStyle(.roundedBorder)
+					.controlSize(.large)
+					.padding(.bottom, 8)
+
+				Text("server.join.egHeader").textCase(.uppercase).font(.headline).opacity(0.75)
+				Text(verbatim: """
+					hTKzmak
+					https://discord.gg/hTKzmak
+					https://discord.gg/cool-people
+					"""
+				)
+			}
+
 			HStack {
 				Button(action: { presented = false }) {
 					Text("action.close")
