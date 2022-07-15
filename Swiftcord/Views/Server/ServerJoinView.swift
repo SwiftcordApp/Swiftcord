3d36b8d02c2f18872cb432b4c681355851d3e4f8
vinkwok <cryptoalgro@gmail.com>
2022-07-15 15:55:35 +0800

    Fix crash by blank invite, handle enter to submit

--- Swiftcord/Views/Server/ServerJoinView.swift
+++ Swiftcord/Views/Server/ServerJoinView.swift
@@ -12,9 +12,50 @@ struct ServerJoinView: View {
 	@Binding var presented: Bool
 
 	@State private var invite = ""
+	@State private var loading = false
 	@State private var error: LocalizedStringKey?
 
 	@EnvironmentObject var rest: DiscordREST
+
+	private func join() async {
+		guard !invite.isEmpty else {
+			withAnimation { error = "server.join.fieldHeader.invalid" }
+			return
+		}
+
+		let id = invite.split(separator: "/").last!
+		AnalyticsWrapper.event(type: .inviteOpened, properties: ["invite_code": id])
+		let resolvedInvite = await rest.resolveInvite(inviteID: String(id), inputValue: invite)
+		AnalyticsWrapper.event(type: .networkInviteResolve, properties: [
+			"code": id,
+			"input_value": invite,
+			"location": "Join Guild"
+		])
+
+		guard let resolvedInvite = resolvedInvite else {
+			withAnimation { error = "server.join.fieldHeader.notFound" }
+			AnalyticsWrapper.event(type: .resolveInvite, properties: [
+				"input_value": invite,
+				"invite_type": "Server Invite",
+				"location": "Join Guild",
+				"code": id,
+				"resolved": false
+			])
+			return
+		}
+		AnalyticsWrapper.event(type: .resolveInvite, properties: [
+			"input_value": invite,
+			"invite_type": "Server Invite",
+			"inviter_id": resolvedInvite.inviter?.id,
+			"location": "Join Guild",
+			"code": id,
+			"resolved": true,
+			"size_online": resolvedInvite.approximate_presence_count,
+			"size_total": resolvedInvite.approximate_member_count
+		])
+		print(resolvedInvite)
+		withAnimation { error = nil }
+	}
 
     var body: some View {
 		DialogView(
@@ -30,43 +71,19 @@ struct ServerJoinView: View {
 			Spacer()
 			Button {
 				Task {
-					let id = invite.split(separator: "/").last!
-					AnalyticsWrapper.event(type: .inviteOpened, properties: ["invite_code": id])
-					let resolvedInvite = await rest.resolveInvite(inviteID: String(id), inputValue: invite)
-					AnalyticsWrapper.event(type: .networkInviteResolve, properties: [
-						"code": id,
-						"input_value": invite,
-						"location": "Join Guild"
-					])
-
-					guard let resolvedInvite = resolvedInvite else {
-						error = "server.join.fieldHeader.notFound"
-						AnalyticsWrapper.event(type: .resolveInvite, properties: [
-							"input_value": invite,
-							"invite_type": "Server Invite",
-							"location": "Join Guild",
-							"code": id,
-							"resolved": false
-						])
-						return
-					}
-					AnalyticsWrapper.event(type: .resolveInvite, properties: [
-						"input_value": invite,
-						"invite_type": "Server Invite",
-						"inviter_id": resolvedInvite.inviter?.id,
-						"location": "Join Guild",
-						"code": id,
-						"resolved": true,
-						"size_online": resolvedInvite.approximate_presence_count,
-						"size_total": resolvedInvite.approximate_member_count
-					])
-					print(resolvedInvite)
-					error = nil
+					withAnimation { loading = true }
+					await join()
+					withAnimation { loading = false }
 				}
 			} label: {
-				Text("server.join.action")
+				if loading {
+					ProgressView().progressViewStyle(.circular).controlSize(.mini)
+				} else {
+					Text("server.join.action")
+				}
 			}
 			.buttonStyle(FlatButtonStyle())
+			.disabled(loading)
 		} content: {
 			Group {
 				if let error = error {
@@ -76,10 +93,16 @@ struct ServerJoinView: View {
 				}
 			}.textCase(.uppercase).font(.headline).opacity(0.75)
 
-			TextField("https://discord.gg/hTKzmak", text: $invite)
-				.textFieldStyle(.roundedBorder)
-				.controlSize(.large)
-				.padding(.bottom, 8)
+			TextField("https://discord.gg/hTKzmak", text: $invite, onCommit: {
+				Task {
+					withAnimation { loading = true }
+					await join()
+					withAnimation { loading = false }
+				}
+			})
+			.textFieldStyle(.roundedBorder)
+			.controlSize(.large)
+			.padding(.bottom, 8)
 
 			Text("server.join.egHeader").textCase(.uppercase).font(.headline).opacity(0.75)
 			Text(verbatim: """
