727e59c6adc9377a1dbdcf068ac5c911642afb16
Teddy Gaillard <teddy.gaillard@icloud.com>
2022-09-06 15:05:23 -0700

    Add image modifier parameter to BetterImageView

--- Swiftcord/Views/Utils/BetterImageView.swift
+++ Swiftcord/Views/Utils/BetterImageView.swift
@@ -10,29 +10,37 @@ import CachedAsyncImage
 
 /// A much better base for remote images, has loading placeholders built in and sane default modifiers for the image
 struct BetterImageView<ErrorContent: View>: View {
-	let url: URL?
-	@ViewBuilder let customErrorView: ErrorContent
-
+    let url: URL?
+    let imageModifier: (Image) -> Image
+    @ViewBuilder let customErrorView: () -> ErrorContent
+    
     var body: some View {
-		CachedAsyncImage(url: url) { phase in
-			if let image = phase.image {
-				image
-					.resizable()
-					.scaledToFill()
-					.transition(.customOpacity)
-			} else if phase.error != nil {
-				customErrorView
-			} else {
-				Rectangle().fill(.gray.opacity(0.25)).transition(.customOpacity)
-			}
-		}
+        CachedAsyncImage(url: url) { phase in
+            if let image = phase.image {
+                imageModifier(image)
+                    .resizable()
+                    .scaledToFill()
+                    .transition(.customOpacity)
+            } else if phase.error != nil {
+                customErrorView()
+            } else {
+                Rectangle().fill(.gray.opacity(0.25)).transition(.customOpacity)
+            }
+        }
     }
 }
 
 extension BetterImageView where ErrorContent == EmptyView {
-	init(url: URL?) {
-		self.init(url: url) {
-			EmptyView()
-		}
-	}
+    init(url: URL?, imageModifier: @escaping (Image) -> Image = { $0 }) {
+        self.init(url: url, imageModifier: imageModifier, customErrorView: {
+            EmptyView()
+        })
+    }
+}
+
+extension BetterImageView {
+    // Allow trailing closure syntax
+    init(url: URL?, @ViewBuilder customErrorView: @escaping () -> ErrorContent) {
+        self.init(url: url, imageModifier: { $0 }, customErrorView: customErrorView)
+    }
 }
